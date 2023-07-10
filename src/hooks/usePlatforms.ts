6984a907d3e9841faf2e04846d26051264cbbe74
kagarwal1811 <kushag203221@gmail.com>
2023-07-10 19:47:11 +0530

    ship static data for genres and platform with app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,12 +1,11 @@
-import useData from "./usedata";
-
+import platform from "../data/platform";
 
 interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => ({ data: platform, error: null });
 
 export default usePlatforms;
